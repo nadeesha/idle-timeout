d62623195b8f429873f1be13394130df7a64dd9e
Nadeesha Cabral <n@nadeesha.me>
2019-02-26 19:05:32 +0530

    Add universal idle timeout features

--- README.md
+++ README.md
@@ -1,26 +1,17 @@
-# [idleTimeout](https://github.com/jackmu95/idle-timeout/)
+# universal-idle-timeout
 
-[![Version](https://badgen.net/npm/v/idle-timeout)](https://www.npmjs.com/package/idle-timeout/)
-[![Size](https://badgen.net/badgesize/normal/jackmu95/idle-timeout/master/dist/idle-timeout.min.js)](https://github.com/jackmu95/idle-timeout/blob/master/dist/idle-timeout.min.js)
-[![License](https://badgen.net/npm/license/idle-timeout)](https://www.npmjs.com/package/idle-timeout/)
-
-A zero dependency, ~5KB library to make idle state detection in the browser an ease. With it's simple but yet powerful API it features everything you will ever need.
+universal-idle-timeout is based on the excellent [idleTimeout](https://github.com/jackmu95/idle-timeout/) which is a zero dependency, ~5KB library to make idle state detection in the browser an ease. universal-idle-timeout maintains a universal user action tracking system across all of the tabs by using localstorage. When localstorage is not available, it gracefully degrades to tracking the actions in the current tab.
 
 ## Installation
 
 ### Using npm
 ```bash
-npm install idle-timeout
+npm install @certsy/universal-idle-timeout
 ```
 
 ### Using yarn
 ```bash
-yarn add idle-timeout
-```
-
-### Using CDN
-```html
-<script src="https://unpkg.com/idle-timeout/dist/idle-timeout.min.js"></script>
+yarn add @certsy/universal-idle-timeout
 ```
 
 
@@ -39,7 +30,6 @@ The idleTimeout constructor takes two arguments:
   * `options [Object]` - _An **optional** options object_
     * `element [Element]` - _The element to listen for the timeout_
     * `timeout [Number]` - _The idle timeout (in milliseconds)_
-    * `loop [Boolean]` - _Wether the timeout should be looped when idle_
 
 ```javascript
 const instance = idleTimeout(
@@ -53,57 +43,3 @@ const instance = idleTimeout(
   }
 );
 ```
-
-### Methods
-`pause()` - _Pauses the timeout_
-```javascript
-instance.pause();
-```
-
-`resume()` - _Resumes an paused timeout_
-```javascript
-instance.resume();
-```
-
-`reset()` - _Reset the timeout_
-```javascript
-instance.reset();
-```
-
-`destroy()` - _Destroy the instance_
-```javascript
-instance.destroy();
-```
-
-### Getters
-`idle [Boolean]` - _Wether the current state is idle_
-```javascript
-instance.idle; // false
-```
-
-### Setters
-`timeout = value [Number]` - _Set the timeout (in milliseconds)_
-```javascript
-instance.timeout = 1000 * 60;
-```
-
-`loop = value [Boolean]` - _Set wether the timeout should be looped_
-```javascript
-instance.loop = true;
-```
-
-`idle = value [Boolean]` - _Set the idle state_
-```javascript
-instance.idle = true;
-```
-
-
-## Browser Support
-
-| Chrome<br><img src="https://cdn.rawgit.com/alrra/browser-logos/master/src/chrome/chrome.svg" width="48" height="48" alt="Chrome"> | Firefox<br><img src="https://cdn.rawgit.com/alrra/browser-logos/master/src/firefox/firefox.svg" width="48" height="48" alt="Firefox"> | Safari<br><img src="https://cdn.rawgit.com/alrra/browser-logos/master/src/safari/safari_128x128.png" width="48" height="48" alt="Safari"> | Opera<br><img src="https://cdn.rawgit.com/alrra/browser-logos/master/src/opera/opera.svg" width="48" height="48" alt="Opera"> | Edge<br><img src="https://cdn.rawgit.com/alrra/browser-logos/master/src/edge/edge.svg" width="48" height="48" alt="Edge"> | IE<br><img src="https://cdn.rawgit.com/alrra/browser-logos/master/src/archive/internet-explorer_9-11/internet-explorer_9-11.svg" width="48" height="48" alt="IE"> |
-| :---: | :---: | :---: | :---: | :---: | :---: |
-| Latest 2 | Latest 2 | Latest 2 | Latest 2 | Latest 2 | 11 |
-
-
-## License
-This project is licensed under the terms of the [MIT License](LICENSE).
--- src/IdleTimeout.ts
+++ src/IdleTimeout.ts
@@ -1,32 +1,24 @@
 import OptionsInterface from './interfaces/Options';
 
-/** Creates an idle timeout instnace. */
 export default class IdleTimeout {
-  /** The callback function to invoke when the timeout is complete. */
+  protected storageKey = "IDLE_TIMEOUT_UNIVERSAL_LAST_ACTION_TIME";
+  
   protected callback: () => void;
 
-  /** The merged configuration options for the timeout. */
   protected options: OptionsInterface;
 
-  /** The timeout handle to reset the timeout. */
   protected timeoutHandle: number | null = null;
 
-  /** Whether the timeout is idle. */
   protected isIdle: boolean = false;
 
-  /** The start time of the timeout in milliseconds. */
   protected startTime: number = 0;
 
-  /** The remaining time of the timeout in milliseconds. */
   protected remainingTime: number = 0;
 
-  /** The last X-axis position on the page. */
   protected lastPageX: number = -1;
 
-  /** The last Y-axis position on the page. */
   protected lastPageY: number = -1;
 
-  /** The input event names. */
   protected eventNames: string[] = [
     'DOMMouseScroll',
     'mousedown',
@@ -40,18 +32,12 @@ export default class IdleTimeout {
     'wheel'
   ];
 
-  /**
-   * The idle timeout constructor.
-   * @param {Function} callback The callback function to invoke when the timeout is complete.
-   * @param {object} [options] The configuration options for the timeout.
-   * @returns {void}
-   */
-  public constructor(callback: () => void, options?: OptionsInterface) {
+  protected activeUniversalActionInThrottle = false;
+
+  public constructor(callback: () => void, options: OptionsInterface) {
     this.callback = callback;
     this.options = {
       element: document.body,
-      loop: false,
-      timeout: 60 * 1000 * 5, // 5 minutes
       ...options
     };
 
@@ -63,66 +49,6 @@ export default class IdleTimeout {
     this.resetTimeout();
   }
 
-  /**
-   * Pause the timeout.
-   * @returns {void}
-   */
-  public pause(): void {
-    const remainingTime: number = this.startTime + this.options.timeout - new Date().getTime();
-    if (remainingTime <= 0) {
-      return;
-    }
-
-    this.remainingTime = remainingTime;
-
-    if (this.timeoutHandle) {
-      clearTimeout(this.timeoutHandle);
-      this.timeoutHandle = null;
-    }
-  }
-
-  /**
-   * Resume the paused timeout.
-   * @returns {void}
-   */
-  public resume(): void {
-    if (this.remainingTime <= 0) {
-      return;
-    }
-
-    this.resetTimeout();
-    this.remainingTime = 0;
-  }
-
-  /**
-   * Reset the timeout.
-   * @returns {void}
-   */
-  public reset(): void {
-    this.isIdle = false;
-    this.remainingTime = 0;
-    this.resetTimeout();
-  }
-
-  /**
-   * Destroy the instance.
-   * @returns {void}
-   */
-  public destroy(): void {
-    const element = this.options.element;
-    this.eventNames.forEach(eventName => {
-      element.removeEventListener(eventName, this.handleEvent);
-    });
-
-    if (this.timeoutHandle) {
-      clearTimeout(this.timeoutHandle);
-    }
-  }
-
-  /**
-   * Reset the timeout function.
-   * @returns {void}
-   */
   protected resetTimeout(): void {
     if (this.timeoutHandle) {
       clearTimeout(this.timeoutHandle);
@@ -166,47 +92,50 @@ export default class IdleTimeout {
     this.resetTimeout();
   };
 
-  /**
-   * Handle the completed timeout.
-   * @returns {void}
-   */
-  protected handleTimeout(): void {
-    this.isIdle = true;
-    this.resetTimeout();
+  protected recordUniversalLastActionTime(): void {
+    if (this.activeUniversalActionInThrottle) {
+      return;
+    }
 
-    this.callback();
+    this.activeUniversalActionInThrottle = true;
+
+    setTimeout(() => {
+      try {
+        window.localStorage.setItem(this.storageKey, Date.now().toString());
+      } catch (e) {
+        // nothing. fail silently
+      } finally {
+        this.activeUniversalActionInThrottle = false;
+      }
+    }, 5000);
   }
 
-  /** Gets whether the timeout is idle. */
+  protected getUniversalLastActionTime(): number {
+    const fallbackTime = 0;
+    
+    try {
+      return Number(window.localStorage.getItem(this.storageKey)) || fallbackTime;
+    } catch (e) {
+      return fallbackTime;
+    }
+  }
+
+  protected handleTimeout(): void {
+    const timeSinceLastUniversalAction = Date.now() - this.getUniversalLastActionTime();
+    const universalActionOccuredDuringTimeout = timeSinceLastUniversalAction < this.options.timeout;
+
+    if (universalActionOccuredDuringTimeout) {
+      this.isIdle = false;
+      this.resetTimeout();
+    } else {
+      this.isIdle = true;
+      this.resetTimeout();
+
+      this.callback();
+    }
+  }
+
   public get idle() {
     return this.isIdle;
-  }
-
-  /**
-   * Sets wether the timeout should restart on completion.
-   * @param {boolean} value Wether the timeout should restart on completion.
-   */
-  public set loop(value: boolean) {
-    this.options.loop = value;
-  }
-
-  /**
-   * Sets the idle timeout in milliseconds.
-   * @param {number} value The idle timeout in milliseconds.
-   */
-  public set timeout(value: number) {
-    this.options.timeout = value;
-  }
-
-  /**
-   * Sets whether the timeout is idle.
-   * @param {boolean} value Whether the timeout is idle.
-   */
-  public set idle(value: boolean) {
-    if (value) {
-      this.handleTimeout();
-    } else {
-      this.reset();
-    }
   }
 }
